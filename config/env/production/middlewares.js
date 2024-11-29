69f9b95c31439da0214872d10ba764535dc4408f
Sumit chauhan <sumitchauhan9807666@gmail.com>
2024-11-30 01:54:06 +0530

    wip

---
+++ config/env/production/middlewares.js
@@ -0,0 +1,22 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'script-src': ['https://cdn.ckeditor.com']
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
