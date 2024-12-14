dfd0b418e07be240b5f0a7ef1546a852c79a7a0c
Sumit chauhan <sumitchauhan9807666@gmail.com>
2024-12-14 08:58:53 +0530

    wip

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -963,6 +963,7 @@ export interface ApiCountryCountry extends Struct.CollectionTypeSchema {
         'ZW:Zimbabwe',
       ]
     >;
+    CountryCode: Schema.Attribute.String;
     createdAt: Schema.Attribute.DateTime;
     updatedAt: Schema.Attribute.DateTime;
     publishedAt: Schema.Attribute.DateTime;
